eac2191c40d17752cfcafe2a3f59e05c99cb42c3
曾学兵 <zengxb94@foxmail.com>
2016-05-12 23:05:08 +0800

    list page ui

---
+++ micro-survery-platform/assets/js/utils.js
@@ -0,0 +1,10 @@
+var $ = {
+    addClass: function (node, addClassName) {
+        var className = node.className;
+        node.className = className + addClassName;
+    },
+    removeClass: function (node, removeClassName) { 
+        var classNames = node.className.split(' ');
+        
+    }
+}
